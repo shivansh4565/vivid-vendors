9af0975958e54f0e42e21ec1e00c89d4dbbaa7a2
shivansh saxena <150766578+shivansh4565@users.noreply.github.com>
2024-10-20 15:09:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Nav from './components/nav';
+import Cards from './components/cards';
+import Cards2 from './components/card2';
+import Photo from './components/photo';
+import Final from './components/final';
+import Footer from './components/footer';
+import './App.css';
+
+function App() {
+  return (
+<>
+<Nav/>
+<Cards/>
+<Cards2/>
+<Photo/>
+<Final/>
+<Footer/>
+
+
+</>
+  );
+}
+
+export default App;
